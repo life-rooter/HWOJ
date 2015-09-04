79d5663096a2f223206da6f8d84c27d3e458e492
banre123 <banre123@126.com>
2015-09-04 20:06:46 +0800

    20150904

---
+++ HW75_Picture.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: HW75_Picture.cpp
+	> Author: xh
+	> Mail: banre123@126.com 
+	> Created Time: Fri 04 Sep 2015 07:56:39 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string s;
+	getline(cin,s);
+	sort(s.begin(),s.end());
+	cout<<s<<endl;
+
+}
