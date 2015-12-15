25f89533998e8ebbefdcef4b5c4e722a4037bc4e
root_xh <banre123@users.noreply.github.com>
2015-12-15 22:43:39 +0800

    Update HW_Chros.cpp

--- HW_Chros.cpp
+++ HW_Chros.cpp
@@ -4,7 +4,144 @@
 	> Mail: banre123@126.com 
 	> Created Time: Mon 07 Sep 2015 08:59:13 PM CST
  ************************************************************************/
+/*
+#include<iostream>
+using namespace std;
+//const int max=100;
+#define max 100
+//const int max=100;//bug: const int can't be the length of array left[] /right[] /a[]?
 
+int main()
+{
+	int n;
+	int a[max];
+	int left[max];
+	int right[max];
+	cin>>n;
+	if(n<=0)
+		return -1;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>a[i] ;
+		if(a[i]<0)
+			return -1;
+	}
+	for(int i=1;i<n;i++)
+	{
+		left[i]=1;
+		for(int j=1;j<i;j++)
+			if(a[i]>a[j]&&left[i]<left[j]+1)
+				left[i]=left[j]+1;
+	}
+	for(int i=n;i>=1;i--)
+	{
+		right[i]=1;
+		for(int j=i+1;j<=n;j++)
+			if(a[i]>a[j]&&right[i]<right[j]+1)
+				right[i]=right[j]+1;
+		}
+	int num=0;
+	for(int i=1;i<=n;i++)
+		if(num<right[i]+left[i]-1)
+			num=right[i]+left[i]-1;
+	cout<<n-num<<endl;
+	
+	system("pause");
+	return 0;
+}
+一测试用例未通过*/ 
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+
+    int len,i;
+
+    cin >> len;
+
+    int *A = new int[len];
+
+    for(int i=0; i<len; ++i)
+
+        cin >> A[i];
+
+
+
+    // lis[i]表示以A[i]为结尾的最长递增子序列的长度
+
+    int *lis = new int[len];  
+
+    // lds[i]表示以A[i]为起点的最长递减子序列的长度
+
+    int *lds = new int[len];
+
+
+
+    for ( i = 0; i < len; ++i) 
+
+    {
+
+        lis[i] = 1;
+
+        lds[i] = 1;
+
+    }
+
+
+
+    for( i=1; i<len; ++i)
+
+        for(int j=0; j<i; ++j)
+
+            if(A[i] > A[j] && lis[i] < lis[j]+1)
+
+                lis[i] = lis[j] + 1;
+
+
+
+    for(i=len-2; i>=0; --i)
+
+        for(int j=len-1; j>i; --j)
+
+            if(A[i] > A[j] && lds[i] < lds[j]+1)
+
+                lds[i] = lds[j] + 1;
+
+
+
+    int maxl = 0;
+
+    for(i=0; i<len; ++i)
+
+        if(maxl < lis[i]+lds[i])
+
+            maxl = lis[i] + lds[i];
+
+
+
+    cout << len - maxl + 1 << endl;
+
+
+    delete [] lis;
+
+    delete [] lds;
+
+    delete [] A;
+
+
+
+    return 0;
+
+}
+
+
+/* wrong!!!!
 #include<iostream>
 #include<vector>
 //#include<algorithm>
@@ -63,3 +200,4 @@ int main()
 	cout<<chros(h)<<endl;
 	//system("pause");
 }
+*/
